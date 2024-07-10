faf55645a2e64d047845929fa540e687332cab0d
laik <laik.lj@me.com>
2024-07-10 22:12:44 +0800

    add 3rdparty deps

--- app/demo/demo.cpp
+++ app/demo/demo.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
-
+//#include <iostream>
+#include <fmt/core.h>
 
 int main(int argc, char *argv[]) {
-    std::cout << "Hello, World!" << std::endl;
+    fmt::print("{}!", "Hello, World");
     return 0;
 }

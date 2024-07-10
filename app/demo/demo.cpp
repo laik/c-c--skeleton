2a006c7ec7204915d0adf6e34a9bdd692b79ba99
laik <laik.lj@me.com>
2024-07-10 22:16:26 +0800

    add 3rdparty deps

--- app/demo/demo.cpp
+++ app/demo/demo.cpp
@@ -1,7 +1,8 @@
 //#include <iostream>
 #include <fmt/core.h>
+#include <fmt/color.h>
 
 int main(int argc, char *argv[]) {
-    fmt::print("{}!", "Hello, World");
+    fmt::print(fmt::fg(fmt::color::crimson), "{}!", "Hello, World");
     return 0;
 }
